f4bdf61db4dd9d55f67ed9732fcba194381a9712
gayatri2211-23 <dahakegayatri2211@gmail.com>
2025-01-02 11:44:34 +0530

    reinitialize

---
+++ src/sections/SectionFirst.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Typography from '../components/Typography';
+import { SharedButton } from '../components/SharedButton';
+
+const SectionFirst = () => {
+    return (
+        <>
+            <div className='bg-gradient--pale-ocean py-5'>
+                <Container>
+                    <Row>
+                        <Col sm={12} md={6}>
+                            <Stack direction="vertical" gap={3} className='justify-content-center align-items-end '>
+                                <Typography variant={'h1'} className={'fs-1 fw-bold text-center'}>
+                                This is for you if you are a parent to a young aspiring cricketer
+                                </Typography>
+                                <Typography variant={'p'} className='fs-5'>Empower your young cricketer to build a stronger self-image and unshakable self-belief with my proven 7-session program—designed to equip them for
+peak performance on the cricket field.</Typography>
+                                <SharedButton variant={'primary'} size={'lg'} className={'btn-primary fw-bold p-3 rounded-pill mx-auto'} label={'Book Your One-On-One Call'} />
+                            </Stack>
+                        </Col>
+                        <Col sm={12} md={6}>
+                            <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" className='w-100 h-100' title="YouTube video" allowfullscreen />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default SectionFirst;
---
+++ src/sections/SectionThird.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+import Typography from '../components/Typography'
+import { Row, Col, Stack, Container } from 'react-bootstrap'
+import { TiTickOutline } from 'react-icons/ti'
+import { SharedButton } from '../components/SharedButton'
+
+const SectionThird = () => {
+  return (
+    <>
+      <div className='py-3'>
+        <Typography variant={'h2'} className={'fw-bold fs-1 text-center'}>
+          This free one on one call is for you if:
+        </Typography>
+        {/* <Container>
+        <hr className='mb-4'/>
+          <Row className='g-5'>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You believe your kid has cricketing potential.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You really want to support him/her in his/her pursuit of this crazy life dream.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your kid to perform to his potential &amp; beyond.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You do not want to see your kid wilt under pressure in crunch situations.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your kid to believe in process
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your kid to have bigger self relief.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your kid to have stronger self image.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your kid to give his/her 100% to this dream.
+                </Typography>
+              </div>
+            </Col>
+            <Col sm={6} md={4}>
+            <div className='element-box'>
+                <Typography variant={'p'} className='fs-5'>
+                You want your aspiring cricketer to build strong foundation to his cricketing career.
+                </Typography>
+              </div>
+            </Col>
+          </Row>
+        <hr className='mt-4'/>
+
+        </Container> */}
+
+        <Container>
+          <Stack direction='vertical' gap={3}>
+        <hr className='mb-4'/>
+
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You believe your kid has cricketing potential.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} /> You really want to support him/her in his/her pursuit of this crazy life dream.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You want your kid to perform to his potential &amp; beyond.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} /> You do not want to see your kid wilt under pressure in crunch situations.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} /> You want your kid to believe in process
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You want your kid to have bigger self relief.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You believe your kid has cricketing potential.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You want your kid to have stronger self image.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} />You want your kid to give his/her 100% to this dream.
+              </Typography>
+            </div>
+            <div className='shadow rounded p-3 '>
+              <Typography variant={'p'} className='fs-5 mb-0'>
+                <TiTickOutline color='#6ED8F0' fontSize={'2rem'} /> You want your aspiring cricketer to build strong foundation to his cricketing career.
+              </Typography>
+            </div>
+        <hr/>
+        <SharedButton variant={'primary'} size={'lg'} className={'btn-primary fw-bold p-3 rounded-pill mx-auto'} label={'Book Your One-On-One Call'} />
+
+          </Stack>
+          
+        </Container>
+      </div>
+
+    </>
+  )
+}
+
+export default SectionThird
+
